a692c15cb565fb296dcb117edd64ddfa13079086
manpreet07 <manpreet1107@gmail.com>
2023-12-03 10:19:57 -0500

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+module.exports = {
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'ember-app',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './App': './src/App',
+      },
+      shared: {},
+    }),
+  ],
+};
